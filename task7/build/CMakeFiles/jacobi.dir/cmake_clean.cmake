faf1bd21ef501f7d8661628996e180f4e28b6520
Kopylov Matvey <kopylovmatveyx@gmail.com>
2025-05-25 19:45:11 +0700

    Lab7

---
+++ task7/build/CMakeFiles/jacobi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jacobi.dir/optimized.cpp.o"
+  "CMakeFiles/jacobi.dir/optimized.cpp.o.d"
+  "jacobi"
+  "jacobi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jacobi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
